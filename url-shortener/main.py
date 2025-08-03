8d02c5fbc507c1074f0d4925db2397fad4a3a7fb
mks_min <mininname@vk.com>
2025-08-03 22:45:41 +0300

    Create base path

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,3 +1,26 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 app = FastAPI(title="URL Shortener")
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="test",
+    )
+    return {
+        "message": f"Hello {name}",
+        "docs": str(docs_url),
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
