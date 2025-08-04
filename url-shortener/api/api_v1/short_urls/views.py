b4fa83e713b4ee0aa412448213fe70fb4507b253
mks_min <mininname@vk.com>
2025-08-04 15:42:54 +0300

    add 404 to responses

--- url-shortener/api/api_v1/short_urls/views.py
+++ url-shortener/api/api_v1/short_urls/views.py
@@ -52,9 +52,23 @@ def read_short_url_details(
 ) -> ShortUrl:
     return url
 
+
 @router.delete(
     "/{slug}/",
     status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Short URL not found",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "URL 'slug' not found",
+                    },
+                },
+            },
+        },
+    },
+)
 def delete_short_url(
     url: Annotated[
         ShortUrl,
