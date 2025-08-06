4c685ecc39fa99624cafe0267fed108ffa241568
mks_min <mininname@vk.com>
2025-08-07 00:03:11 +0300

    Add Bearer Auth Token

--- url-shortener/api/api_v1/short_urls/dependencies.py
+++ url-shortener/api/api_v1/short_urls/dependencies.py
@@ -7,6 +7,11 @@ from fastapi import (
     Request,
     status,
     Header,
+    Depends,
+)
+from fastapi.security import (
+    HTTPAuthorizationCredentials,
+    HTTPBearer,
 )
 
 from .crud import storage
@@ -21,6 +26,11 @@ UNSAFE_METHODS = frozenset(
         "DELETE",
         "PATCH",
     }
+)
+static_api_token = HTTPBearer(
+    scheme_name="Static API token",
+    description="Your **Static API token** from the developer portal. [Read more](https://ya.ru)",
+    auto_error=False,
 )
 
 
@@ -49,16 +59,21 @@ def save_storage_state(
 def api_token_required_for_unsafe_methods(
     request: Request,
     api_token: Annotated[
-        str,
-        Header(
-            alias="x-auth-token",
-        ),
-    ] = "",
+        HTTPAuthorizationCredentials | None,
+        Depends(static_api_token),
+    ] = None,
 ):
+    log.info("API token: %s", api_token)
     if request.method not in UNSAFE_METHODS:
         return
 
-    if api_token not in API_TOKENS:
+    if not api_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API token is required",
+        )
+
+    if api_token.credentials not in API_TOKENS:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid API token",
