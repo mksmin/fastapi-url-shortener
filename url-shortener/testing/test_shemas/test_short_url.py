4e2a46f016cfdf69f3bac638c5646ba52ce5ab33
mks_min <mininname@vk.com>
2025-08-11 14:25:09 +0300

    Schemas test: compatibility base and create

---
+++ url-shortener/testing/test_shemas/test_short_url.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from schemas.short_url import ShortUrl, ShortUrlCreate
+
+
+class ShortUrlCreateTestCase(TestCase):
+    def test_short_url_can_be_created_from_create_schema(self) -> None:
+        short_url_in = ShortUrlCreate(
+            slug="some-slug",
+            description="some-description",
+            target_url="https://example.com",
+        )
+
+        short_url = ShortUrl(
+            **short_url_in.model_dump(),
+        )
+        self.assertEqual(
+            short_url_in.target_url,
+            short_url.target_url,
+        )
+        self.assertEqual(
+            short_url_in.slug,
+            short_url.slug,
+        )
+        self.assertEqual(
+            short_url_in.description,
+            short_url.description,
+        )
