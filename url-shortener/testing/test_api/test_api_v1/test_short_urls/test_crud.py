ecec01951a512a5a4306da035b0a9f007256c472
mks_min <mininname@vk.com>
2025-08-14 15:03:05 +0300

    CRUD Tests: setUpClass, tearDownClass, gets short_urls

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -1,35 +1,42 @@
 import random
 import string
 from os import getenv
+from typing import ClassVar
 from unittest import TestCase
 
 from api.api_v1.short_urls.crud import storage
-from schemas.short_url import ShortUrl, ShortUrlPartialUpdate, ShortUrlUpdate
+from schemas.short_url import (
+    ShortUrl,
+    ShortUrlCreate,
+    ShortUrlPartialUpdate,
+    ShortUrlUpdate,
+)
 
 if getenv("TESTING") != "1":
     msg = "Environment is not ready for testing"
     raise EnvironmentError(msg)  # noqa: UP024
 
 
+def create_short_url() -> ShortUrl:
+    short_url_in = ShortUrlCreate(
+        slug="".join(
+            random.choices(
+                string.ascii_letters,
+                k=8,
+            ),
+        ),
+        description="test-description",
+        target_url="https://example.com",
+    )
+    return storage.create(short_url_in)
+
+
 class ShortUrlStorageUpdateTestCase(TestCase):
     def setUp(self) -> None:
-        self.short_url = self.create_short_url()
+        self.short_url = create_short_url()
 
     def tearDown(self) -> None:
         storage.delete(self.short_url)
-
-    def create_short_url(self) -> ShortUrl:
-        short_url_in = ShortUrl(
-            slug="".join(
-                random.choices(
-                    string.ascii_letters,
-                    k=8,
-                ),
-            ),
-            description="test-description",
-            target_url="https://example.com",
-        )
-        return storage.create(short_url_in)
 
     def test_update(self) -> None:
         short_url_update = ShortUrlUpdate(
@@ -71,3 +78,37 @@ class ShortUrlStorageUpdateTestCase(TestCase):
             short_url_partial_update.description,
             updated_short_url.description,
         )
+
+
+class ShortUrlStorageGetShortUrlsTestCase(TestCase):
+    SHORT_URLS_COUNT = 3
+    short_urls: ClassVar[list[ShortUrl]] = []
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.short_urls = [create_short_url() for _ in range(cls.SHORT_URLS_COUNT)]
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        for short_url in cls.short_urls:
+            storage.delete(short_url)
+
+    def test_get_list(self) -> None:
+        short_urls = storage.get()
+        expected_slugs = {su.slug for su in self.short_urls}
+        slugs = {su.slug for su in short_urls}
+        expected_diff = set[str]()
+        diff = expected_slugs - slugs
+        self.assertEqual(expected_diff, diff)
+
+    def test_by_slug(self) -> None:
+        for short_url in self.short_urls:
+            with self.subTest(
+                slug=short_url.slug,
+                msg=f"Validate can be slug {short_url.slug!r}",
+            ):
+                db_short_url = storage.get_by_slug(short_url.slug)
+                self.assertEqual(
+                    short_url,
+                    db_short_url,
+                )
