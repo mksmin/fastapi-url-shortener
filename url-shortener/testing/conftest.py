dedb3fa1fe7f4543f477964ce3b5a7da85d7707c
mks_min <mininname@vk.com>
2025-08-20 00:05:15 +0300

    Create parametrized fixture test for short url create invalid

--- url-shortener/testing/conftest.py
+++ url-shortener/testing/conftest.py
@@ -13,22 +13,37 @@ if getenv("TESTING") != "1":
     pytest.exit(msg)
 
 
-def create_short_url() -> ShortUrl:
-    short_url_in = ShortUrlCreate(
+def build_short_url(slug: str) -> ShortUrlCreate:
+    return ShortUrlCreate(
+        slug=slug,
+        description="test-description",
+        target_url="https://example.com",
+    )
+
+
+def build_short_url_random_slug() -> ShortUrlCreate:
+    return build_short_url(
         slug="".join(
             random.choices(  # noqa: S311
                 string.ascii_letters,
                 k=8,
             ),
         ),
-        description="test-description",
-        target_url="https://example.com",
     )
+
+
+def create_short_url(slug: str) -> ShortUrl:
+    short_url_in = build_short_url(slug=slug)
+    return storage.create(short_url_in)
+
+
+def create_short_url_random_slug() -> ShortUrl:
+    short_url_in = build_short_url_random_slug()
     return storage.create(short_url_in)
 
 
 @pytest.fixture()
 def short_url() -> Generator[ShortUrl, None, None]:
-    short_url = create_short_url()
+    short_url = create_short_url_random_slug()
     yield short_url
     storage.delete(short_url)
--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_create.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_create.py
@@ -1,11 +1,15 @@
 import random
 import string
+from typing import Any
 
+import pytest
+from _pytest.fixtures import SubRequest
 from fastapi import status
 from fastapi.testclient import TestClient
 
 from main import app
 from schemas.short_url import ShortUrl, ShortUrlCreate
+from testing.conftest import build_short_url_random_slug
 
 
 def test_create_short_url(auth_client: TestClient) -> None:
@@ -40,3 +44,39 @@ def test_create_short_url_already_exists(
     assert response.status_code == status.HTTP_409_CONFLICT, response.text
     expected_error_detail = f"Short url with slug='{short_url.slug}' already exists"
     assert response.json()["detail"] == expected_error_detail, response.json()
+
+
+class TestCreateInvalid:
+    @pytest.fixture(
+        params=[
+            pytest.param(("", "string_too_short"), id="empty slug"),
+            pytest.param(("ab", "string_too_short"), id="slug too short"),
+            pytest.param(("qwerty-1234", "string_too_long"), id="slug too long"),
+        ],
+    )
+    def short_url_create_values(
+        self,
+        request: SubRequest,
+    ) -> tuple[dict[str, Any], str]:
+        build = build_short_url_random_slug()
+        data = build.model_dump(mode="json")
+        slug, err_type = request.param
+        data["slug"] = slug
+        return data, err_type
+
+    def test_invalid_slug(
+        self,
+        short_url_create_values: tuple[dict[str, Any], str],
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("create_short_url")
+        create_data, expected_error_type = short_url_create_values
+        response = auth_client.post(
+            url=url,
+            json=create_data,
+        )
+        assert (
+            response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        ), response.text
+        error_detail = response.json()["detail"][0]
+        assert error_detail["type"] == expected_error_type, error_detail
