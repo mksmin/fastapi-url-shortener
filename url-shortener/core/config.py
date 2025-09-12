193ba2f62c7aaf4fdbf3316c4b8a048cab1a0b81
mks_min <mininname@vk.com>
2025-09-12 14:12:06 +0300

    Using yaml files for config app

--- url-shortener/core/config.py
+++ url-shortener/core/config.py
@@ -3,7 +3,12 @@ from pathlib import Path
 from typing import Literal, Self
 
 from pydantic import BaseModel, model_validator
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOG_FORMAT = (
@@ -72,7 +77,43 @@ class Settings(BaseSettings):
         ),
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
+        yaml_file=(
+            BASE_DIR / "config.default.yaml",
+            BASE_DIR / "config.local.yaml",
+        ),
+        yaml_config_section="url-shortener",
     )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        """
+        Define the sources and their order for loading the settings values.
+
+        Args:
+            settings_cls: The Settings class.
+            init_settings: The `InitSettingsSource` instance.
+            env_settings: The `EnvSettingsSource` instance.
+            dotenv_settings: The `DotEnvSettingsSource` instance.
+            file_secret_settings: The `SecretsSettingsSource` instance.
+
+        Returns:
+            A tuple containing the sources and their order
+            for loading the settings values.
+        """
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            YamlConfigSettingsSource(settings_cls),
+        )
 
     logging: LoggingConfig = LoggingConfig()
     redis: RedisConfig = RedisConfig()
