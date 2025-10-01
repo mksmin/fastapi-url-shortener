3ead4adda764b7346ed475be95295f488029d74e
mks_min <mininname@vk.com>
2025-10-01 17:32:50 +0300

    using jinja templates

--- url-shortener/api/main_views.py
+++ url-shortener/api/main_views.py
@@ -1,16 +1,39 @@
+from datetime import date
+
 from fastapi import (
     APIRouter,
+    Request,
 )
 from fastapi.responses import HTMLResponse
 
-from core.config import BASE_DIR
+from templating import templates
 
 router = APIRouter()
 
 
 @router.get(
     "/",
-    response_class=HTMLResponse,
+    include_in_schema=False,
 )
-def read_root() -> str:
-    return (BASE_DIR / "pages" / "home.html").read_text()
+def read_root(
+    request: Request,
+) -> HTMLResponse:
+    context = {}
+    today = date.today()
+    features = [
+        "Create short URLs",
+        "Track all redirects",
+        "Real-time statistics",
+        "Shared management",
+    ]
+
+    context.update(
+        today=today,
+        features=features,
+    )
+
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context=context,
+    )
---
+++ url-shortener/templating/jinja_templates.py
@@ -0,0 +1,7 @@
+from fastapi.templating import Jinja2Templates
+
+from core.config import BASE_DIR
+
+templates = Jinja2Templates(
+    directory=BASE_DIR / "templates",
+)
