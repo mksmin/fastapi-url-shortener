a37a7de481a6b6b1685a9cb8174774d83e353df9
mks_min <mininname@vk.com>
2025-10-02 21:08:52 +0300

    Update tests for home view

--- url-shortener/testing/test_api/test_main_views.py
+++ url-shortener/testing/test_api/test_main_views.py
@@ -2,32 +2,12 @@ import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
+pytestmark = pytest.mark.templatetest
+
 
 def test_root_view(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK, response.text
-    response_data = response.json()
-    expected_message = "Hello World"
-    assert response_data["message"] == expected_message, response_data
-
-
-@pytest.mark.parametrize(
-    "name",
-    [
-        # TODO: fake data
-        "John",
-        "",
-        "John Smith",
-        "!@#$%^&*()_+",
-    ],
-)
-def test_root_view_custom_name(
-    name: str,
-    client: TestClient,
-) -> None:
-    query = {"name": name}
-    response = client.get("/", params=query)
-    assert response.status_code == status.HTTP_200_OK, response.text
-    response_data = response.json()
-    expected_message = f"Hello {name}"
-    assert response_data["message"] == expected_message, response_data
+    assert response.template.name == "home.html"  # type: ignore[attr-defined]
+    assert "features" in response.context, response.context  # type: ignore[attr-defined]
+    assert isinstance(response.context["features"], list)  # type: ignore[attr-defined]
