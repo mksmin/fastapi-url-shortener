527db4afb162cdcb04a381da84ea0e0a313435b8
mks_min <mininname@vk.com>
2025-10-12 13:32:29 +0300

    Required basic auth on form submit

--- url-shortener/rest/short_urls/__init__.py
+++ url-shortener/rest/short_urls/__init__.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from dependencies.auth import user_basic_auth_required_for_unsafe_methods
 from rest.short_urls.create_views import router as create_views_router
 from rest.short_urls.list_views import router as list_views_router
 
 router = APIRouter(
     tags=["Short URLs rest"],
     prefix="/short-urls",
+    dependencies=[Depends(user_basic_auth_required_for_unsafe_methods)],
 )
 router.include_router(list_views_router)
 router.include_router(create_views_router)
